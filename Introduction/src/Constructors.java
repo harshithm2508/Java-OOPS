8adde829159de305b63f8b0332697a336bf478b4
Harshith Muthangi <vsharshith25@gmail.com>
2024-01-23 06:49:54 +0530

    added method in class

--- Introduction/src/Constructors.java
+++ Introduction/src/Constructors.java
@@ -1,10 +1,18 @@
 public class Constructors {
     public static void main(String[] args) {
-        Car ferrari = new Car();
-        System.out.println(ferrari.brand);
-        System.out.println(ferrari.engine);
-        System.out.println(ferrari.seats);
-        System.out.println(ferrari.wheels);
+        Car lambo = new Car();
+        System.out.println(lambo.brand);
+        System.out.println(lambo.engine);
+        System.out.println(lambo.seats);
+        System.out.println(lambo.wheels);
+
+
+        Car mclaren = new Car(4,2,"McLaren","M840T");
+        System.out.println(mclaren.wheels);
+        System.out.println(mclaren.seats);
+        System.out.println(mclaren.brand);
+        System.out.println(mclaren.engine);
+        mclaren.description();
     }
 }
 
@@ -17,10 +25,22 @@ class Car{
 
 
     //  This constructor will be called when you use 'new Car()' overriding all the default values declared in the class.
+    //  The below constructor is itself the Default Constructor.
     Car(){
         this.wheels = 5;
         this.seats = 3;
         this.brand = "Lamborghini";
         this.engine = "Diesel";
+    }
+
+    Car(int wheels, int seats, String brand, String engine){
+        this.wheels = wheels;
+        this.seats = seats;
+        this.brand = brand;
+        this.engine = engine;
+    }
+
+    void description(){
+        System.out.println("I am "+this.brand+" with "+this.wheels+" wheels and "+this.seats+" seats. I have a powerful "+this.engine+" engine.");
     }
 }
